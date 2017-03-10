dbf20b69c5731790da83b0ca88e889702a66f5b2
FalconRay <falconray@yahoo.com>
2017-03-10 01:19:42 -0800

    add nfs server installation script

---
+++ 1604Dsk/nfsServer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#[install nfs server]
+sudo apt install nfs-kernel-server
+#sudo vim /etc/exports
+#/opt/ums    *(rw,sync,no_root_squash,no_subtree_check)
+sudo systemctl start nfs-kernel-server.service
+#//Mac mount: sudo mount -t nfs -o rw,resvport 172.16.197.180:/opt/ums ums
+
