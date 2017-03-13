f41efed54612acb74c84367f2c852f1c9deb51b0
FalconRay <falconray@yahoo.com>
2017-03-12 22:19:02 -0700

    add local kcp tunnel setup script

---
+++ 1604Dsk/kcpLaunch/kcp.sh
@@ -0,0 +1,8 @@
+./client_linux_amd64 -r "23.106.154.150:14369" -l ":47140" -mode fast2 &
+./client_linux_amd64 -r "23.106.154.150:47369" -l ":47141" -mode fast2 &
+./client_linux_amd64 -r "23.106.154.150:17369" -l ":47142" -mode fast2 &
+./client_linux_amd64 -r "23.106.154.150:25369" -l ":47143" -mode fast2 &
+./client_linux_amd64 -r "23.106.154.150:58369" -l ":47144" -mode fast2 &
+./client_linux_amd64 -r "23.106.154.150:28369" -l ":47145" -mode fast2 &
+./client_linux_amd64 -r "23.106.154.150:36369" -l ":47146" -mode fast2 &
+./client_linux_amd64 -r "23.106.154.150:39369" -l ":47147" -mode fast2 &
