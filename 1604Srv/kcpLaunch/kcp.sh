af5ff3dd626b5b8c98147b6a937cc8e82e64eb97
FalconRay <falconray@yahoo.com>
2017-03-13 01:13:43 -0400

    add kcp tunnel setup script

---
+++ 1604Srv/kcpLaunch/kcp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#/usr/local/kcp-server/kcp-server -l ":47369" -t "127.0.0.1:47147" --key "&Fuckgfw" &
+/opt/gows/src/github.com/xtaci/kcptun/server_linux_amd64 -t "127.0.0.1:47147" -l ":14369" -mode fast2 &
+/opt/gows/src/github.com/xtaci/kcptun/server_linux_amd64 -t "127.0.0.1:47147" -l ":47369" -mode fast2 &
+/opt/gows/src/github.com/xtaci/kcptun/server_linux_amd64 -t "127.0.0.1:47147" -l ":17369" -mode fast2 &
+/opt/gows/src/github.com/xtaci/kcptun/server_linux_amd64 -t "127.0.0.1:47147" -l ":25369" -mode fast2 &
+/opt/gows/src/github.com/xtaci/kcptun/server_linux_amd64 -t "127.0.0.1:47147" -l ":58369" -mode fast2 &
+/opt/gows/src/github.com/xtaci/kcptun/server_linux_amd64 -t "127.0.0.1:47147" -l ":28369" -mode fast2 &
+/opt/gows/src/github.com/xtaci/kcptun/server_linux_amd64 -t "127.0.0.1:47147" -l ":36369" -mode fast2 &
+/opt/gows/src/github.com/xtaci/kcptun/server_linux_amd64 -t "127.0.0.1:47147" -l ":39369" -mode fast2 &
+
---
+++ 1604Srv/kcpLaunch/ss.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ss-server -s 0.0.0.0 -p 47147 -l 2080 -k "&Fuckgfw" -m aes-256-cfb -a root -u -f /var/run/rw47147.pid&
+
+
