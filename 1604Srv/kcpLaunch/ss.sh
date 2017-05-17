249d5530fec28f8289987bca3528d226dac326dc
FalconRay <falconray@yahoo.com>
2017-05-16 21:21:16 -0400

    open 10 ss-server as default

--- 1604Srv/kcpLaunch/ss.sh
+++ 1604Srv/kcpLaunch/ss.sh
@@ -20,6 +20,16 @@ stop()
 
 start()
 {
+	ss-server -s 0.0.0.0 -p 1000 -l 2081 -k "&Fuckgfw" -m aes-256-cfb -a root -u -f /var/run/rw1000.pid&
+	ss-server -s 0.0.0.0 -p 2000 -l 2082 -k "&Fuckgfw" -m aes-256-cfb -a root -u -f /var/run/rw2000.pid&
+	ss-server -s 0.0.0.0 -p 3000 -l 2083 -k "&Fuckgfw" -m aes-256-cfb -a root -u -f /var/run/rw3000.pid&
+	ss-server -s 0.0.0.0 -p 4000 -l 2084 -k "&Fuckgfw" -m aes-256-cfb -a root -u -f /var/run/rw4000.pid&
+	ss-server -s 0.0.0.0 -p 5000 -l 2085 -k "&Fuckgfw" -m aes-256-cfb -a root -u -f /var/run/rw5000.pid&
+	ss-server -s 0.0.0.0 -p 6000 -l 2086 -k "&Fuckgfw" -m aes-256-cfb -a root -u -f /var/run/rw6000.pid&
+	ss-server -s 0.0.0.0 -p 7000 -l 2087 -k "&Fuckgfw" -m aes-256-cfb -a root -u -f /var/run/rw7000.pid&
+	ss-server -s 0.0.0.0 -p 8000 -l 2088 -k "&Fuckgfw" -m aes-256-cfb -a root -u -f /var/run/rw8000.pid&
+	ss-server -s 0.0.0.0 -p 9000 -l 2089 -k "&Fuckgfw" -m aes-256-cfb -a root -u -f /var/run/rw9000.pid&
+
 	ss-server -s 0.0.0.0 -p 10000 -l 2080 -k "&Fuckgfw" -m aes-256-cfb -a root -u -f /var/run/rw10000.pid&
 }
 
