87986d0ee8c942ee793d9ef5dbaf245fce4fbd67
FalconRay <falconray@yahoo.com>
2017-06-26 19:02:08 +0800

    kcp client launch deploy for pi3

--- pi3/initDeploy.sh
+++ pi3/initDeploy.sh
@@ -19,12 +19,12 @@ wget -c https://github.com/falconray0704/pkgsBak/raw/master/ss/v2.6.2/release/bi
 tar -jxf binV2.6.2_arm.tar.bz2
 sudo cp -a /opt/etmp/binV2.6.2/* /usr/local/bin/
 rm -rf /opt/etmp/binV2.6.2
-cp /opt/github/ubuntuCfg/1604Srv/kcpLaunch/ss.sh ~/
+#cp /opt/github/ubuntuCfg/1604Srv/kcpLaunch/ss.sh ~/
 
 #deploy kcpserver
 wget -c https://github.com/falconray0704/pkgsBak/raw/master/kcp/release/kcptun-linux-arm-20170319.tar.gz
 tar -zxf kcptun-linux-arm-20170319.tar.gz
 sudo mv client_linux_arm7 /usr/local/bin/
 sudo mv server_linux_arm7 /usr/local/bin/
-cp /opt/github/ubuntuCfg/1604Srv/kcpLaunch/kcp.sh ~/
+cp /opt/github/ubuntuCfg/pi3/launch/kcp.sh ~/
 
