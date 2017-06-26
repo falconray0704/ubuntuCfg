9b5bbcd9565809fd1a53f7c39ac49c2cb66d1f1f
FalconRay <falconray@yahoo.com>
2017-06-26 18:01:36 +0800

    pi3 init deploy

---
+++ pi3/general.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install automake autogen autoconf
+sudo apt-get install build-essential
+sudo apt-get install git wget curl
+sudo apt-get install tree htop
+#sudo apt-get install openssh-server
+
---
+++ pi3/samba.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+#[install samba server]
+sudo apt-get install samba
+#sudo vim /etc/samba/smb.conf
+sudo systemctl restart smbd.service nmbd.service 
+#//Mac mount: mount_smbfs //ray:ray@172.16.197.180/ums ums
+
