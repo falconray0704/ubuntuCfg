fb693c536b0b392f89f6ed2ea9e1dee07cfaca74
FalconRay <falconray@yahoo.com>
2017-07-07 19:19:43 -0700

    refine init deploy server

--- 1604Srv/initDeploy.sh
+++ 1604Srv/initDeploy.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get dist-upgrade
-sudo apt-get install build-essential
-sudo apt-get install automake autogen autoconf
-sudo apt-get install zlib1g-dev
-sudo apt-get install libssl-dev
-sudo apt-get install libpcre3 libpcre3-dev
-sudo apt-get install asciidoc
-sudo apt-get install git
-sudo apt-get install wget curl
-sudo apt-get install htop
+sudo apt-get -y update && apt-get -y dist-upgrade && apt-get -y install build-essential automake autogen autoconf zlib1g-dev libssl-dev libpcre3 libpcre3-dev asciidoc git wget curl htop
 
 mkdir -p /opt/github
 mkdir -p /opt/etmp
@@ -17,14 +8,14 @@ mkdir -p /opt/etmp
 cd /opt/etmp
 wget -c https://github.com/falconray0704/pkgsBak/raw/master/ss/v2.6.2/release/binV2.6.2.tar.bz2
 tar -jxf binV2.6.2.tar.bz2
-cp -a /opt/etmp/binV2.6.2/* /usr/local/bin/
+sudo cp -a /opt/etmp/binV2.6.2/* /usr/local/bin/
 rm -rf /opt/etmp/binV2.6.2
 cp /opt/github/ubuntuCfg/1604Srv/kcpLaunch/ss.sh ~/
 
 #deploy kcpserver
 wget -c https://github.com/falconray0704/pkgsBak/raw/master/kcp/release/kcptun-linux-amd64-20170319.tar.gz
 tar -zxf kcptun-linux-amd64-20170319.tar.gz
-mv client_linux_amd64 /usr/local/bin/
-mv server_linux_amd64 /usr/local/bin/
+sudo mv client_linux_amd64 /usr/local/bin/
+sudo mv server_linux_amd64 /usr/local/bin/
 cp /opt/github/ubuntuCfg/1604Srv/kcpLaunch/kcp.sh ~/
 
