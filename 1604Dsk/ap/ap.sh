80e7f35ebbbbb79238b8fbc036c5df0f18871dab
FalconRay <falconray@yahoo.com>
2017-07-19 19:18:09 -0700

    make install function inject start up AP forward iptable rules.

--- 1604Dsk/ap/ap.sh
+++ 1604Dsk/ap/ap.sh
@@ -5,7 +5,36 @@ apName=wlan0
 apSSID=piAP
 apPwd=piAP
 
+ssIP="127.0.0.1"
+ssPort=9000
+
+sstPort=9001
+sstListenPort=1020
+
 outInterface=eth0
+
+get_ssArgs()
+{
+	echo "Please input your SS server IP:"
+	read ssIP
+	echo "Please input your SS server Port:"
+	read ssPort
+	echo "Please input your ss-tunnel Port:"
+	read ssPort
+
+	echo "Your SS IP:${ssIP} Port:${ssPort} ss-tunnel Port:${sstPort}"
+
+	echo "Is it correct? [y/N]"
+	read isCorrect
+
+	if [ ${isCorrect}x = "Y"x ] || [ ${isCorrect}x = "y"x ]; then
+		echo "Continue to config iptable rules...."
+	else
+		#echo "incorrect"
+		exit 1
+	fi
+
+}
 
 get_args()
 {
@@ -49,8 +78,10 @@ unmanaged_devices()
 	echo "[keyfile]" >> ./tmpConfigs/NetworkManager.conf 
 	echo "unmanaged-devices=mac:${apMac}" >> ./tmpConfigs/NetworkManager.conf 
 	
+	echo "============= after config ./tmpConfigs/NetworkManager.conf start ====================="
 	cat ./tmpConfigs/NetworkManager.conf
-	sudo cp ./tmpConfigs/NetworkManager.conf /etc/NetworkManager/NetworkManager.conf
+	echo "============= after config ./tmpConfigs/NetworkManager.conf end   ====================="
+	#sudo cp ./tmpConfigs/NetworkManager.conf /etc/NetworkManager/NetworkManager.conf
 }
 
 hostapd_config()
@@ -63,9 +94,11 @@ hostapd_config()
 	sed -i "s/ssid=piAP/ssid=${apSSID}/g" ./tmpConfigs/hostapd.conf
 	sed -i "s/wpa_passphrase=piAP/wpa_passphrase=${apPwd}/g" ./tmpConfigs/hostapd.conf
 
+	echo "================ after config ./tmpConfigs/hostapd.conf start ============================"
 	cat ./tmpConfigs/hostapd.conf
+	echo "================ after config ./tmpConfigs/hostapd.conf end   ============================"
 
-	sudo cp ./tmpConfigs/hostapd.conf /etc/hostapd/
+	#sudo cp ./tmpConfigs/hostapd.conf /etc/hostapd/
 }
 
 dnsmasq_config()
@@ -75,19 +108,73 @@ dnsmasq_config()
 	cp configs/dnsmasq_AP.conf ./tmpConfigs/
 	sed -i "s/interface=wlan0/interface=${apName}/g" ./tmpConfigs/dnsmasq_AP.conf
 	
+	echo "================= after config ./tmpConfigs/dnsmasq_AP.conf start ======================="
 	cat ./tmpConfigs/dnsmasq_AP.conf
+	echo "================= after config ./tmpConfigs/dnsmasq_AP.conf end   ======================="
 
-	sudo cp ./tmpConfigs/dnsmasq_AP.conf /etc/
+	#sudo cp ./tmpConfigs/dnsmasq_AP.conf /etc/
+}
+
+enableAP_ss_forward()
+{
+
+	sudo iptables -t nat -N SHADOWSOCKS
+
+	sudo iptables -t nat -A SHADOWSOCKS -d ${ssIP}-j RETURN
+	sudo iptables -t nat -A SHADOWSOCKS -d 0.0.0.0/8 -j RETURN
+	sudo iptables -t nat -A SHADOWSOCKS -d 10.0.0.0/8 -j RETURN
+	sudo iptables -t nat -A SHADOWSOCKS -d 127.0.0.0/8 -j RETURN
+	sudo iptables -t nat -A SHADOWSOCKS -d 169.254.0.0/16 -j RETURN
+	sudo iptables -t nat -A SHADOWSOCKS -d 172.16.0.0/12 -j RETURN
+	sudo iptables -t nat -A SHADOWSOCKS -d 192.168.0.0/16 -j RETURN
+	sudo iptables -t nat -A SHADOWSOCKS -d 224.0.0.0/4 -j RETURN
+	sudo iptables -t nat -A SHADOWSOCKS -d 240.0.0.0/4 -j RETURN
+
+	sudo iptables -t nat -A SHADOWSOCKS -p tcp -j REDIRECT --to-ports ${ssPort}
+	sudo iptables -t nat -A PREROUTING -p tcp -j SHADOWSOCKS
+	sudo iptables -t nat -A OUTPUT -p tcp -j SHADOWSOCKS
+
+
+	sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
+
+	#sudo echo 'iptables-restore < /etc/iptables.ipv4.nat' >> /etc/rc.local
+	#sudo echo 'up iptables-restore < /etc/iptables.ipv4.nat' >> /etc/network/interfaces
+
+}
+
+sstunel_config()
+{
+	sed -i "s/1.1.1.1/${ssIP}/g" ./tmpConfigs/AP.service
+	sed -i "s/9001/${ssPort}/g" ./tmpConfigs/AP.service
+	#sed -i "s/1020/${sstPort}/g" ./tmpConfigs/AP.service
+}
+
+ss_config()
+{
+	get_ssArgs
+	sstunel_config
 }
 
 encapsulate_service()
 {
 	cp configs/AP.service ./tmpConfigs/
 	sed -i "s/wlan0/${apName}/g" ./tmpConfigs/AP.service
-	
-	cat ./tmpConfigs/AP.service
 
-	sudo cp ./tmpConfigs/AP.service /lib/systemd/system/
+
+	echo "Do you want to config SS?[y/N]:"
+	read isConfigSS
+
+	if [ ${isConfigSS}x = "Y"x ] || [ ${isConfigSS}x = "y"x ]; then
+		ss_config
+	else
+		sed -i '/ss-tunnel -s/d' ./tmpConfigs/AP.service
+	fi
+	
+	echo "==================== after cconfig ./tmpConfigs/AP.service start ==================="
+	cat ./tmpConfigs/AP.service
+	echo "==================== after cconfig ./tmpConfigs/AP.service end   ==================="
+
+	#sudo cp ./tmpConfigs/AP.service /lib/systemd/system/
 
 }
 
@@ -150,9 +237,18 @@ enableAP_forward_startup()
 
 	sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
 
+	sed -i '/iptables-restore < /etc/iptables.ipv4.nat/d' /etc/network/interfaces
 	#sudo echo 'iptables-restore < /etc/iptables.ipv4.nat' >> /etc/rc.local
 	sudo echo 'up iptables-restore < /etc/iptables.ipv4.nat' >> /etc/network/interfaces
 
+}
+
+commit_all_configs()
+{
+	sudo cp ./tmpConfigs/NetworkManager.conf /etc/NetworkManager/NetworkManager.conf
+	sudo cp ./tmpConfigs/hostapd.conf /etc/hostapd/
+	sudo cp ./tmpConfigs/dnsmasq_AP.conf /etc/
+	sudo cp ./tmpConfigs/AP.service /lib/systemd/system/
 }
 
 if [ $UID -ne 0 ]
@@ -174,7 +270,10 @@ case $1 in
 		unmanaged_devices
 		hostapd_config
 		dnsmasq_config
+		enableAP_forward_startup
 		encapsulate_service
+
+		commit_all_configs
 
 		echo "Please review configuration before enable AP service."
 		echo "Are those correct and reboot for continue?"
@@ -199,7 +298,7 @@ case $1 in
 		enableAP_forward_startup
 	;;
 	"check") echo "Checking AP services..."
-		ps -ef | grep -E ".*hostapd|.*dnsmasq" | grep -v grep
+		ps -ef | grep -E ".*hostapd|.*dnsmasq|.*ss-tunel" | grep -v grep
 	;;
 	"test") echo "test command..."
 		#unmanaged_devices
