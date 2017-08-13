9058f9999bc8efed9382b0fcd76e1ad0417f2bd3
FalconRay <falconray@yahoo.com>
2017-08-13 01:51:47 +0100

    make multi ss server

--- 1604Srv/kcpLaunch/ss.sh
+++ 1604Srv/kcpLaunch/ss.sh
@@ -30,7 +30,16 @@ start()
 	ss-server -s 0.0.0.0 -p 54008 -l 2088 -k "&Fuckgfw" -m aes-256-cfb -a root --fast-open -u -f /var/run/rw54008.pid&
 	ss-server -s 0.0.0.0 -p 54009 -l 2089 -k "&Fuckgfw" -m aes-256-cfb -a root --fast-open -u -f /var/run/rw54009.pid&
 
-	ss-server -s 0.0.0.0 -p 10000 -l 2080 -k "&Fuckgfw" -m aes-256-cfb -a root --fast-open -u -f /var/run/rw10000.pid&
+	ss-server -s 0.0.0.0 -p 10000 -l 3080 -k "&Fuckgfw" -m aes-256-cfb -a root --fast-open -u -f /var/run/rw10000.pid&
+	ss-server -s 0.0.0.0 -p 10001 -l 3081 -k "&Fuckgfw" -m aes-256-cfb -a root --fast-open -u -f /var/run/rw10001.pid&
+	ss-server -s 0.0.0.0 -p 10002 -l 3082 -k "&Fuckgfw" -m aes-256-cfb -a root --fast-open -u -f /var/run/rw10002.pid&
+	ss-server -s 0.0.0.0 -p 10003 -l 3083 -k "&Fuckgfw" -m aes-256-cfb -a root --fast-open -u -f /var/run/rw10003.pid&
+	ss-server -s 0.0.0.0 -p 10004 -l 3084 -k "&Fuckgfw" -m aes-256-cfb -a root --fast-open -u -f /var/run/rw10004.pid&
+	ss-server -s 0.0.0.0 -p 10005 -l 3085 -k "&Fuckgfw" -m aes-256-cfb -a root --fast-open -u -f /var/run/rw10005.pid&
+	ss-server -s 0.0.0.0 -p 10006 -l 3086 -k "&Fuckgfw" -m aes-256-cfb -a root --fast-open -u -f /var/run/rw10006.pid&
+	ss-server -s 0.0.0.0 -p 10007 -l 3087 -k "&Fuckgfw" -m aes-256-cfb -a root --fast-open -u -f /var/run/rw10007.pid&
+	ss-server -s 0.0.0.0 -p 10008 -l 3088 -k "&Fuckgfw" -m aes-256-cfb -a root --fast-open -u -f /var/run/rw10008.pid&
+	ss-server -s 0.0.0.0 -p 10009 -l 3089 -k "&Fuckgfw" -m aes-256-cfb -a root --fast-open -u -f /var/run/rw10009.pid&
 }
 
 case $1 in
