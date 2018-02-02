6bf41a1ab2ef8b8a2a94abfd217f906ad864b6aa
FalconRay <falconray@yahoo.com>
2018-02-01 16:27:05 -0800

    update go install to version 1.9.3

--- 1604Dsk/go.sh
+++ 1604Dsk/go.sh
@@ -3,9 +3,9 @@
 sudo apt-get install gccgo-5
 
 cd ~/
-export http_proxy=192.168.152.1:42581
-export https_proxy=192.168.152.1:42581
-export sock5_proxy=192.168.152.1:42581
+export http_proxy=10.1.51.48:42581
+export https_proxy=10.1.51.48:42581
+export sock5_proxy=10.1.51.48:42581
 rm -rf go go1.4
 
 wget -c https://storage.googleapis.com/golang/go1.4-bootstrap-20161024.tar.gz
@@ -17,9 +17,10 @@ GOROOT_BOOTSTRAP=/usr ./make.bash
 cd ~
 #git clone https://github.com/golang/go.git
 #wget https://storage.googleapis.com/golang/go1.8.src.tar.gz
-wget -c https://storage.googleapis.com/golang/go1.9.1.src.tar.gz
+#wget -c https://storage.googleapis.com/golang/go1.9.1.src.tar.gz
+wget -c https://dl.google.com/go/go1.9.3.src.tar.gz
 #tar -zxf go1.8.src.tar.gz
-tar -zxf go1.9.1.src.tar.gz
+tar -zxf go1.9.3.src.tar.gz
 cd go/src
 ./all.bash
 
