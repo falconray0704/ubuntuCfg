662e3d3af2be6b7f3c767dfb4622995653ee6daa
FalconRay <falconray@yahoo.com>
2018-02-12 14:58:45 +0800

    disable guest session in lightdm

--- 1604Dsk/dskEnv.sh
+++ 1604Dsk/dskEnv.sh
@@ -3,3 +3,8 @@
 
 sudo apt-get install unity-tweak-tool
 
+# disable guest account in lightdm
+sudo mkdir -p /etc/lightdm/lightdm.conf.d
+cd /etc/lightdm/lightdm.conf.d/
+sudo sh -c 'printf "[SeatDefaults]\nallow-guest=false\n" > /etc/lightdm/lightdm.conf.d/50-no-guest.conf'
+
