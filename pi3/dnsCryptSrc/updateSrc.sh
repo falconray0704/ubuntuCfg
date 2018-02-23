627d412f178c04f5003577fc035738da5016198a
FalconRay <falconray@yahoo.com>
2018-02-22 21:57:57 -0800

    add dns crypt source backup

---
+++ pi3/dnsCryptSrc/updateSrc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget -c https://download.dnscrypt.info/dnscrypt-resolvers/v2/public-resolvers.md
+wget -c https://download.dnscrypt.info/dnscrypt-resolvers/v2/opennic.md
+wget -c https://mastad0n.github.io/resolvers/v2/usa.md
+
