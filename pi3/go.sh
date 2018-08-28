4d8a35081584eddc8b90912f7a73ba2e554f6daf
FalconRay <falconray@yahoo.com>
2018-08-28 16:20:22 +0800

    refine go.sh's operations' interaction.

--- pi3/go.sh
+++ pi3/go.sh
@@ -8,9 +8,9 @@ set -o errexit
 #set -o xtrace
 
 
-sudo apt-get install gccgo-5
+sudo apt-get -y install gccgo-5
 
-cd ~/
+pushd ~/
 #export http_proxy=10.1.51.48:42581
 #export https_proxy=10.1.51.48:42581
 #export sock5_proxy=10.1.51.48:42581
@@ -19,18 +19,22 @@ rm -rf go go1.4
 wget -c https://dl.google.com/go/go1.4-bootstrap-20171003.tar.gz
 tar -zxf go1.4-bootstrap-20171003.tar.gz
 mv go go1.4
-cd ~/go1.4/src
+pushd ~/go1.4/src
 sudo update-alternatives --set go /usr/bin/go-5
 GOROOT_BOOTSTRAP=/usr ./make.bash
-cd ~
+popd
+
 #git clone https://github.com/golang/go.git
 #wget https://storage.googleapis.com/golang/go1.8.src.tar.gz
 #wget -c https://storage.googleapis.com/golang/go1.9.1.src.tar.gz
 wget -c https://dl.google.com/go/go1.10.3.src.tar.gz
 #tar -zxf go1.8.src.tar.gz
 tar -zxf go1.10.3.src.tar.gz
-cd go/src
+pushd go/src
 ./all.bash
+popd
+
+popd
 
 # set go env
 #export GOROOT=$HOME/go
