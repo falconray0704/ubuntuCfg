a8c94f5bbc2ecda712e4df579c6b2c507493e10b
FalconRay <falconray@yahoo.com>
2018-08-28 13:27:44 +0800

    add dist-upgrade for pi general.sh

--- pi3/general.sh
+++ pi3/general.sh
@@ -8,6 +8,7 @@ set -o errexit
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
 sudo apt-get -y install automake autogen autoconf
 sudo apt-get -y install build-essential
 sudo apt-get -y install git wget curl
