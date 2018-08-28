527c58ee6eeda75d8b7b43ce35cd98bf27166fc8
FalconRay <falconray@yahoo.com>
2018-08-28 15:44:18 +0800

    refine pi3 vim.sh's error process with exit approach.

---
+++ pi3/vim.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+# trace each command execute, same as `bash -v myscripts.sh`
+#set -o verbose
+# trace each command execute with attachment infomations, same as `bash -x myscripts.sh`
+#set -o xtrace
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install automake autogen autoconf build-essential
+sudo apt-get -y install git wget curl tree
+
+sudo apt-get install vim
+sudo apt-get install ctags
+cd ~/
+rm -rf .vimrc vim generate.vim
+
+echo "Fetch generate.vim from http://www.vim-bootstrap.com/"
+echo "mv generate.vim ~/.vimrc
+
+echo "exec following command manually:"
+echo "vim +PlugInstall +qall"
+
