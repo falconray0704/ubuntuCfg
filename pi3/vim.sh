d34b91c4101a5bab1f77eac9da231bdfb0518bd1
FalconRay <falconray@yahoo.com>
2018-08-28 16:07:39 +0800

    refine interaction of pi's vim deployment

--- pi3/vim.sh
+++ pi3/vim.sh
@@ -12,13 +12,13 @@ sudo apt-get -y upgrade
 sudo apt-get -y install automake autogen autoconf build-essential
 sudo apt-get -y install git wget curl tree
 
-sudo apt-get install vim
-sudo apt-get install ctags
+sudo apt-get -y install vim
+sudo apt-get -y install ctags
 cd ~/
 rm -rf .vimrc vim generate.vim
 
 echo "Fetch generate.vim from http://www.vim-bootstrap.com/"
-echo "mv generate.vim ~/.vimrc
+echo "mv generate.vim ~/.vimrc"
 
 echo "exec following command manually:"
 echo "vim +PlugInstall +qall"
