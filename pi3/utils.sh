a1bcdabb8c577b90dab8a7f4434cf42ab65c9c40
FalconRay <falconray@yahoo.com>
2018-08-28 12:08:15 +0800

    refine pi3 utils.sh's error process with exit approach.

--- pi3/utils.sh
+++ pi3/utils.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+set -o nounset
+set -o errexit
+# trace each command execute, same as `bash -v myscripts.sh`
+#set -o verbose
+# trace each command execute with attachment infomations, same as `bash -x myscripts.sh`
+#set -o xtrace
+
 
 restore_from_image_func()
 {
