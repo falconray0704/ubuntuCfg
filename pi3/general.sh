8f479a36d6af118aef0b75261a4816991b54e802
FalconRay <falconray@yahoo.com>
2018-08-28 13:48:35 +0800

    deploy latest grpc in pi's general.sh.

--- pi3/general.sh
+++ pi3/general.sh
@@ -6,6 +6,31 @@ set -o errexit
 # trace each command execute with attachment infomations, same as `bash -x myscripts.sh`
 #set -o xtrace
 
+
+install_Latest_grpc()
+{
+    sudo apt-get -y install build-essential autoconf libtool pkg-config
+    sudo apt-get -y install libgflags-dev libgtest-dev
+    sudo apt-get -y install clang libc++-dev
+
+    pushd /opt/github
+    #git clone https://github.com/grpc/grpc
+    git clone -b $(curl -L https://grpc.io/release) https://github.com/grpc/grpc
+    pushd grpc
+    git submodule update --init
+    make
+    sudo make install
+
+    # install protobuf
+    pushd third_party/protobuf
+    sudo make install
+    popd
+
+    popd
+    popd
+}
+
+
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
@@ -13,5 +38,9 @@ sudo apt-get -y install automake autogen autoconf
 sudo apt-get -y install build-essential
 sudo apt-get -y install git wget curl
 sudo apt-get -y install tree htop vim
+sudo apt-get -y install libicu-dev libboost-all-dev libncurses5-dev
 #sudo apt-get -y install openssh-server
+
+install_Latest_grpc
+
 
