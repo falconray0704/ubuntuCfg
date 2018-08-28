1189df07fc6083cce463b668f32d89c29dbacd02
FalconRay <falconray@yahoo.com>
2018-08-28 15:28:55 +0800

    refine pi3 initDeploy.sh's error process with exit approach.

--- pi3/initDeploy.sh
+++ pi3/initDeploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+set -o nounset
+set -o errexit
+# trace each command execute, same as `bash -v myscripts.sh`
+#set -o verbose
+# trace each command execute with attachment infomations, same as `bash -x myscripts.sh`
+#set -o xtrace
 
 install_basic_tools()
 {
-	sudo chown -hR ray /opt
-	sudo chgrp -hR ray /opt
-
 	sudo apt-get -y update && sudo apt-get -y dist-upgrade
 	sudo apt-get -y install build-essential automake autogen autoconf zlib1g-dev libssl-dev libpcre3 libpcre3-dev asciidoc git wget curl htop
 
@@ -13,23 +16,21 @@ install_basic_tools()
 #deploy ss
 deploy_ss_func()
 {
-	mkdir -p /opt/github
-	mkdir -p /opt/etmp
-	cd /opt/etmp
+	pushd /opt/etmp
 
 	wget -c https://github.com/falconray0704/pkgsBak/raw/master/ss/v2.6.2/release/binV2.6.2_arm.tar.bz2
 	tar -jxf binV2.6.2_arm.tar.bz2
 	sudo cp -a /opt/etmp/binV2.6.2/* /usr/local/bin/
 	rm -rf /opt/etmp/binV2.6.2
 	#cp /opt/github/ubuntuCfg/1604Srv/kcpLaunch/ss.sh ~/
+
+    popd
 }
 
 #deploy kcp
 deploy_kcp_func()
 {
-	mkdir -p /opt/github
-	mkdir -p /opt/etmp
-	cd /opt/etmp
+	pushd /opt/etmp
 
 	wget -c https://github.com/falconray0704/pkgsBak/raw/master/kcp/release/kcptun-linux-arm-20170319.tar.gz
 	tar -zxf kcptun-linux-arm-20170319.tar.gz
@@ -37,6 +38,8 @@ deploy_kcp_func()
 	sudo mv server_linux_arm7 /usr/local/bin/
 
 	cp /opt/github/ubuntuCfg/pi3/launch/kcp.sh ~/
+
+    popd
 }
 
 enable_hwclock_func()
@@ -45,6 +48,20 @@ enable_hwclock_func()
 	sudo sed -i 's/^#dtoverlay=lirc-rpi$/dtoverlay=i2c-rtc,ds1307/' /boot/config.txt
 	sudo sed -i '/exit 0/i\hwclock -s' /etc/rc.local
 }
+
+init_operation_dirs_func()
+{
+    mkdir -p /opt/github
+	mkdir -p /opt/github/falconray0704
+    mkdir -p /opt/etmp
+}
+
+test_func()
+{
+    echo "logingName:${USER}"
+}
+
+init_operation_dirs_func
 
 case $1 in
 	"enable_hwclock") echo "hwclock enable..."
@@ -62,8 +79,11 @@ case $1 in
 	;;
 	"deploy_all") echo "Deploying all..."
 		install_basic_tools
-		deploy_kcp_func
+		#deploy_kcp_func
 	;;
+    "test") echo "Testing ..."
+        test_func
+    ;;
 	*) echo "unknow cmd"
 esac
 
