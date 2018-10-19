041cd27a33b40f65ced510e6f52ace9da5e88026
FalconRay <falconray@yahoo.com>
2018-10-19 15:38:25 +0800

    nodejs development env deploy

---
+++ nodejs.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+
+VERSION_NVM="v0.33.11"
+
+tips_nrm_func()
+{
+    echo "List all npm source: "
+    echo "npm install -g nrm"
+
+}
+
+install_nrm_func()
+{
+    npm install -g nrm
+}
+
+install_nodejs_func()
+{
+    nvm install v4.9.1
+    nvm install v6.14.4
+    nvm install v8.12.0
+    nvm install v10.12.0
+}
+
+install_nvm_func()
+{
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/${VERSION_NVM}/install.sh | bash
+    nvm --version
+}
+
+
+case $1 in
+    installNvm) echo "Installing nvm ${VERSION_NVM}..."
+        install_nvm_func
+        ;;
+    installNodejs) echo "Installing nodejs..."
+        install_nodejs_func
+        ;;
+    installNrm) echo "Installing nrm..."
+        install_nrm_func
+        ;;
+    tipsNrm) echo "Tips nrm:"
+        tips_nrm_func
+        ;;
+    *) echo "Unknown cmd: $1"
+esac
+
+
